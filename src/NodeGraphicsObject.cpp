0de329d4f63d2219de22dd88e45c20db98d37b0e
Malcolm Tyrrell <Malcolm.R.Tyrrell@gmail.com>
2025-05-17 09:03:12 +0100

    Remove dropshadow until QT bug resolved.

--- src/NodeGraphicsObject.cpp
+++ src/NodeGraphicsObject.cpp
@@ -37,14 +37,15 @@ NodeGraphicsObject::NodeGraphicsObject(BasicGraphicsScene &scene, NodeId nodeId)
 
     NodeStyle nodeStyle(nodeStyleJson);
 
-    {
-        auto effect = new QGraphicsDropShadowEffect;
-        effect->setOffset(4, 4);
-        effect->setBlurRadius(20);
-        effect->setColor(nodeStyle.ShadowColor);
-
-        setGraphicsEffect(effect);
-    }
+    // Disabled until QTBUG-77400 resolved
+    //{
+    //    auto effect = new QGraphicsDropShadowEffect;
+    //    effect->setOffset(4, 4);
+    //    effect->setBlurRadius(20);
+    //    effect->setColor(nodeStyle.ShadowColor);
+    //
+    //    setGraphicsEffect(effect);
+    //}
 
     setOpacity(nodeStyle.Opacity);
 
